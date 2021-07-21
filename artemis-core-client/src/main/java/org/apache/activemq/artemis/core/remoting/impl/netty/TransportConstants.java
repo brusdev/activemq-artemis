507d52be62608af868f4ca509f2b32f942ee8f23
Domenico Francesco Bruscino <brusdev@apache.org>
2021-07-21 11:16:20 +0200

    ARTEMIS-3367 Set verifyHost parameter true by default

--- artemis-core-client/src/main/java/org/apache/activemq/artemis/core/remoting/impl/netty/TransportConstants.java
+++ artemis-core-client/src/main/java/org/apache/activemq/artemis/core/remoting/impl/netty/TransportConstants.java
@@ -246,7 +246,7 @@ public class TransportConstants {
 
    public static final boolean DEFAULT_WANT_CLIENT_AUTH = false;
 
-   public static final boolean DEFAULT_VERIFY_HOST = false;
+   public static final boolean DEFAULT_VERIFY_HOST = true;
 
    public static final String DEFAULT_SSL_PROVIDER = "JDK";
 
