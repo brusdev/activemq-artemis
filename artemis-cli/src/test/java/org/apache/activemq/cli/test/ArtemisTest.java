brusdev/activemq-artemis

commit f8b045bd2d95ec28c70c4d64c85400b28d8baef5

ARTEMIS-3522 Implement performance tool for RUL benchmarks
Co-authored-by: gtully

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/messages/ConnectionAbstract.java
@@ -43,10 +43,10 @@ public class ConnectionAbstract extends InputAbstract {
    protected String password;
 
    @Option(name = "--clientID", description = "ClientID to be associated with connection")
-   String clientID;
+   protected String clientID;
 
    @Option(name = "--protocol", description = "Protocol used. Valid values are amqp or core. Default=core.")
-   String protocol = "core";
+   protected String protocol = "core";
 
    public String getBrokerURL() {
       return brokerURL;
@@ -126,16 +126,27 @@ public class ConnectionAbstract extends InputAbstract {
    }
 
    protected ConnectionFactory createConnectionFactory() throws Exception {
+      return createConnectionFactory(brokerURL, user, password, clientID, protocol);
+   }
+
+   protected ConnectionFactory createConnectionFactory(String brokerURL,
+                                                       String user,
+                                                       String password,
+                                                       String clientID,
+                                                       String protocol) throws Exception {
       if (protocol.equals("core")) {
-         return createCoreConnectionFactory();
+         return createCoreConnectionFactory(brokerURL, user, password, clientID);
       } else if (protocol.equals("amqp")) {
-         return createAMQPConnectionFactory();
+         return createAMQPConnectionFactory(brokerURL, user, password, clientID);
       } else {
          throw new IllegalStateException("protocol " + protocol + " not supported");
       }
    }
 
-   private ConnectionFactory createAMQPConnectionFactory() {
+   private ConnectionFactory createAMQPConnectionFactory(String brokerURL,
+                                                         String user,
+                                                         String password,
+                                                         String clientID) {
       if (brokerURL.startsWith("tcp://")) {
          // replacing tcp:// by amqp://
          brokerURL = "amqp" + brokerURL.substring(3);
@@ -172,8 +183,14 @@ public class ConnectionAbstract extends InputAbstract {
    }
 
    protected ActiveMQConnectionFactory createCoreConnectionFactory() {
-      ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL, user, password);
+      return createCoreConnectionFactory(brokerURL, user, password, clientID);
+   }
 
+   protected ActiveMQConnectionFactory createCoreConnectionFactory(String brokerURL,
+                                                                   String user,
+                                                                   String password,
+                                                                   String clientID) {
+      ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL, user, password);
       if (clientID != null) {
          System.out.println("Consumer:: clientID = " + clientID);
          cf.setClientID(clientID);

artemis-cli/pom.xml
@@ -113,6 +113,10 @@
          <artifactId>commons-lang3</artifactId>
       </dependency>
       <dependency>
+         <groupId>org.hdrhistogram</groupId>
+         <artifactId>HdrHistogram</artifactId>
+      </dependency>
+      <dependency>
          <groupId>org.apache.commons</groupId>
          <artifactId>commons-configuration2</artifactId>
          <scope>test</scope>

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/Artemis.java
@@ -37,6 +37,9 @@ import org.apache.activemq.artemis.cli.commands.check.HelpCheck;
 import org.apache.activemq.artemis.cli.commands.check.NodeCheck;
 import org.apache.activemq.artemis.cli.commands.check.QueueCheck;
 import org.apache.activemq.artemis.cli.commands.messages.Transfer;
+import org.apache.activemq.artemis.cli.commands.messages.perf.PerfClientCommand;
+import org.apache.activemq.artemis.cli.commands.messages.perf.PerfConsumerCommand;
+import org.apache.activemq.artemis.cli.commands.messages.perf.PerfProducerCommand;
 import org.apache.activemq.artemis.cli.commands.queue.StatQueue;
 import org.apache.activemq.artemis.cli.commands.Run;
 import org.apache.activemq.artemis.cli.commands.Stop;
@@ -162,6 +165,10 @@ public class Artemis {
       Cli.CliBuilder<Action> builder = Cli.<Action>builder("artemis").withDescription("ActiveMQ Artemis Command Line").
          withCommand(HelpAction.class).withCommand(Producer.class).withCommand(Transfer.class).withCommand(Consumer.class).
          withCommand(Browse.class).withCommand(Mask.class).withCommand(PrintVersion.class).withDefaultCommand(HelpAction.class);
+
+      builder.withGroup("perf").withDescription("Perf tools group (example ./artemis perf client)")
+         .withDefaultCommand(PerfClientCommand.class)
+         .withCommands(PerfProducerCommand.class, PerfConsumerCommand.class, PerfClientCommand.class);
 
       builder.withGroup("check").withDescription("Check tools group (node|queue) (example ./artemis check node)").
          withDefaultCommand(HelpCheck.class).withCommands(NodeCheck.class, QueueCheck.class);

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/messages/DestAbstract.java
@@ -65,13 +65,17 @@ public class DestAbstract extends ConnectionAbstract {
    }
 
    protected Destination getDestination(Session session) throws JMSException {
+      return getDestination(session, destination);
+   }
+
+   public static Destination getDestination(Session session, String destination) throws JMSException {
       if (destination.startsWith(ActiveMQDestination.TOPIC_QUALIFIED_PREFIX)) {
          return session.createTopic(stripPrefix(destination));
       }
       return session.createQueue(stripPrefix(destination));
    }
 
-   private String stripPrefix(String destination) {
+   public static String stripPrefix(String destination) {
       int index = destination.indexOf("://");
       if (index != -1) {
          return destination.substring(index + 3);

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/messages/perf/AsyncJms2ProducerFacade.java
@@ -1,0 +1,274 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.cli.commands.messages.perf;
+
+import javax.jms.BytesMessage;
+import javax.jms.CompletionListener;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
+import static java.util.Objects.requireNonNull;
+
+public final class AsyncJms2ProducerFacade {
+
+   private final long id;
+   protected final Session session;
+   private final MessageProducer producer;
+
+   /*
+    * maxPending limits the number of in-flight sent messages
+    * in a way that if the limit is reached and a single completion arrive,
+    * a subsequent send attempt will succeed.
+    */
+   private long pending;
+   private final long maxPending;
+
+   /*
+    * Tracking sent messages in transaction requires using 2 separate counters
+    * ie pendingMsgInTransaction, completedMsgInTransaction
+    * because, using just one won't allow tracking completions of previously sent messages in order to commit
+    * the transaction while there are no more in-flight ones.
+    */
+   private final long transactionCapacity;
+   private long pendingMsgInTransaction;
+   private long completedMsgInTransaction;
+
+   private final List<Runnable> availableObservers;
+   private final List<Runnable> closedObservers;
+
+   private static final AtomicLongFieldUpdater<AsyncJms2ProducerFacade> MESSAGE_SENT_UPDATER = AtomicLongFieldUpdater.newUpdater(AsyncJms2ProducerFacade.class, "messageSent");
+   private static final AtomicLongFieldUpdater<AsyncJms2ProducerFacade> MESSAGE_COMPLETED_UPDATER = AtomicLongFieldUpdater.newUpdater(AsyncJms2ProducerFacade.class, "messageCompleted");
+   private static final AtomicLongFieldUpdater<AsyncJms2ProducerFacade> NOT_AVAILABLE_UPDATER = AtomicLongFieldUpdater.newUpdater(AsyncJms2ProducerFacade.class, "notAvailable");
+
+   private volatile long messageSent;
+   private volatile long messageCompleted;
+   private volatile long notAvailable;
+
+   private boolean closing;
+   private boolean closed;
+   private final Destination destination;
+
+   public AsyncJms2ProducerFacade(final long id,
+                                  final Session session,
+                                  final MessageProducer producer,
+                                  final Destination destination,
+                                  final long maxPending,
+                                  final long transactionCapacity) {
+      this.id = id;
+      this.session = requireNonNull(session);
+      this.producer = requireNonNull(producer);
+      this.destination = destination;
+      this.pending = 0;
+      this.maxPending = transactionCapacity > 0 && maxPending > 0 ? Math.max(maxPending, transactionCapacity) : maxPending;
+      this.availableObservers = new ArrayList<>(1);
+      this.closedObservers = new ArrayList<>(1);
+      this.messageSent = 0;
+      this.messageCompleted = 0;
+      this.notAvailable = 0;
+      try {
+         if (transactionCapacity < 0) {
+            throw new IllegalStateException("transactionCapacity must be >= 0");
+         }
+         if (transactionCapacity > 0) {
+            if (!session.getTransacted()) {
+               throw new IllegalStateException("session must be transacted with transactionCapacity != 0");
+            }
+         } else {
+            if (session.getTransacted()) {
+               throw new IllegalStateException("session cannot be transacted with transactionCapacity = 0");
+            }
+         }
+      } catch (final JMSException ex) {
+         throw new IllegalStateException(ex);
+      }
+      this.transactionCapacity = transactionCapacity;
+      this.pendingMsgInTransaction = 0;
+      this.completedMsgInTransaction = 0;
+      this.closing = false;
+      this.closed = false;
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public Destination getDestination() {
+      return destination;
+   }
+
+   BytesMessage createBytesMessage() throws JMSException {
+      return session.createBytesMessage();
+   }
+
+   private void addedPendingSend() {
+      if (transactionCapacity > 0 && pendingMsgInTransaction == transactionCapacity) {
+         throw new IllegalStateException("reached max in-flight transacted sent messages");
+      }
+      if (maxPending > 0 && pending == maxPending) {
+         throw new IllegalStateException("reached max in-flight sent messages");
+      }
+      pending++;
+      pendingMsgInTransaction++;
+   }
+
+   /**
+    * if {@code true}, a subsequent {@link #trySend} would return {@link SendAttemptResult#Success}.<br>
+    * Otherwise, a subsequent {@link #trySend} would return {@link SendAttemptResult#NotAvailable}.
+    */
+   private boolean isAvailable() {
+      if (maxPending > 0 && pending == maxPending) {
+         return false;
+      }
+      return transactionCapacity == 0 || pendingMsgInTransaction != transactionCapacity;
+   }
+
+   public enum SendAttemptResult {
+      Closing, Closed, NotAvailable, Success
+   }
+
+   public SendAttemptResult trySend(final Message message,
+                                    final CompletionListener completionListener,
+                                    final Runnable availableObserver) throws JMSException {
+      if (closing) {
+         return SendAttemptResult.Closing;
+      }
+      if (closed) {
+         return SendAttemptResult.Closed;
+      }
+      if (!isAvailable()) {
+         availableObservers.add(availableObserver);
+         orderedIncrementNotAvailable();
+         return SendAttemptResult.NotAvailable;
+      }
+      producer.send(message, completionListener);
+      orderedIncrementSent();
+      addedPendingSend();
+      return SendAttemptResult.Success;
+   }
+
+   public void onSendErrored() {
+      if (closed) {
+         return;
+      }
+      availableObservers.clear();
+      closedObservers.forEach(Runnable::run);
+      closedObservers.clear();
+      closed = true;
+   }
+
+   public JMSException onSendCompleted() {
+      if (closed) {
+         return null;
+      }
+      JMSException completionError = null;
+      orderedIncrementCompleted();
+      if (transactionCapacity > 0 && completedMsgInTransaction == transactionCapacity) {
+         throw new IllegalStateException("cannot complete more send");
+      }
+      if (pending == 0) {
+         throw new IllegalStateException("cannot complete more send");
+      }
+      pending--;
+      completedMsgInTransaction++;
+      if (transactionCapacity > 0) {
+         if (completedMsgInTransaction == transactionCapacity || (closing && pending == 0)) {
+            completedMsgInTransaction = 0;
+            pendingMsgInTransaction = 0;
+            try {
+               session.commit();
+            } catch (final JMSException fatal) {
+               completionError = fatal;
+               closing = true;
+            }
+            if (closing) {
+               closing = false;
+               closed = true;
+               closedObservers.forEach(Runnable::run);
+               closedObservers.clear();
+            } else if (isAvailable()) {
+               availableObservers.forEach(Runnable::run);
+               availableObservers.clear();
+            }
+         }
+      } else {
+         if (closing && pending == 0) {
+            closing = false;
+            closed = true;
+            closedObservers.forEach(Runnable::run);
+            closedObservers.clear();
+         } else if (isAvailable()) {
+            availableObservers.forEach(Runnable::run);
+            availableObservers.clear();
+         }
+      }
+      return completionError;
+   }
+
+   public long getMessageSent() {
+      return messageSent;
+   }
+
+   private void orderedIncrementSent() {
+      MESSAGE_SENT_UPDATER.lazySet(this, messageSent + 1);
+   }
+
+   public long getMessageCompleted() {
+      return messageCompleted;
+   }
+
+   private void orderedIncrementCompleted() {
+      MESSAGE_COMPLETED_UPDATER.lazySet(this, messageCompleted + 1);
+   }
+
+   public long getNotAvailable() {
+      return notAvailable;
+   }
+
+   private void orderedIncrementNotAvailable() {
+      NOT_AVAILABLE_UPDATER.lazySet(this, notAvailable + 1);
+   }
+
+   public void requestClose() {
+      requestClose(() -> {
+      });
+   }
+
+   public void requestClose(final Runnable onClosed) {
+      if (closed) {
+         onClosed.run();
+         return;
+      }
+      if (closing) {
+         closedObservers.add(onClosed);
+         return;
+      }
+      availableObservers.clear();
+      if (pending > 0) {
+         closing = true;
+         closedObservers.add(onClosed);
+      } else {
+         closed = true;
+         onClosed.run();
+      }
+   }
+}

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/messages/perf/BenchmarkService.java
@@ -1,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.cli.commands.messages.perf;
+
+public interface BenchmarkService extends AutoCloseable {
+
+   BenchmarkService start();
+
+   boolean anyError();
+
+   boolean isRunning();
+
+   @Override
+   void close();
+
+}

commit 318b43b91ffc1882ab574424ea919490a7f85283

ARTEMIS-3886 fix CLI operation retry
Commit f8b045bd2d95ec28c70c4d64c85400b28d8baef5 broke the operation
retry as it introduced local variables named "user" and "password" which
overried the class-level variables of the same name. Therefore, when the
user re-enters the username and password on the command-line those
values won't actually be used when attempting to reconnect.

artemis-cli/src/main/java/org/apache/activemq/artemis/cli/commands/messages/ConnectionAbstract.java
@@ -164,7 +164,7 @@ public class ConnectionAbstract extends InputAbstract {
          // if a security exception will get the user and password through an input
          context.err.println("Connection failed::" + e.getMessage());
          userPassword();
-         cf = new JmsConnectionFactory(user, password, brokerURL);
+         cf = new JmsConnectionFactory(this.user, this.password, brokerURL);
          if (clientID != null) {
             cf.setClientID(clientID);
          }
@@ -174,7 +174,7 @@ public class ConnectionAbstract extends InputAbstract {
          context.err.println("Connection failed::" + e.getMessage());
          brokerURL = input("--url", "Type in the broker URL for a retry (e.g. tcp://localhost:61616)", brokerURL);
          userPassword();
-         cf = new JmsConnectionFactory(user, password, brokerURL);
+         cf = new JmsConnectionFactory(this.user, this.password, brokerURL);
          if (clientID != null) {
             cf.setClientID(clientID);
          }
@@ -205,7 +205,7 @@ public class ConnectionAbstract extends InputAbstract {
             context.err.println("Connection failed::" + e.getMessage());
          }
          userPassword();
-         cf = new ActiveMQConnectionFactory(brokerURL, user, password);
+         cf = new ActiveMQConnectionFactory(brokerURL, this.user, this.password);
          if (clientID != null) {
             cf.setClientID(clientID);
          }
@@ -217,7 +217,7 @@ public class ConnectionAbstract extends InputAbstract {
          }
          brokerURL = input("--url", "Type in the broker URL for a retry (e.g. tcp://localhost:61616)", brokerURL);
          userPassword();
-         cf = new ActiveMQConnectionFactory(brokerURL, user, password);
+         cf = new ActiveMQConnectionFactory(brokerURL, this.user, this.password);
          if (clientID != null) {
             cf.setClientID(clientID);
          }

artemis-cli/src/test/java/org/apache/activemq/cli/test/ArtemisTest.java
@@ -40,6 +40,7 @@ import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
@@ -1264,6 +1265,38 @@ public class ArtemisTest extends CliTestBase {
    @Test
    public void testSimpleRun() throws Exception {
       testSimpleRun("server");
+   }
+
+   @Test
+   public void testOperationRetry() throws Exception {
+      File instanceFolder = temporaryFolder.newFolder("server");
+      setupAuth(instanceFolder);
+      Run.setEmbedded(true);
+      Artemis.main("create", instanceFolder.getAbsolutePath(), "--verbose", "--force", "--silent", "--no-web", "--queues", "q1", "--no-autotune", "--require-login", "--default-port", "61616");
+      System.setProperty("artemis.instance", instanceFolder.getAbsolutePath());
+
+      try {
+         Artemis.internalExecute("run");
+         InputStream in = new ByteArrayInputStream("admin\n".getBytes());
+         ActionContext context = new ActionContext(in, System.out, System.err);
+
+         /*
+          * This operation should fail the first time and then prompt the user to re-enter the username which
+          * it will read from the InputStream in the ActionContext. It can't read the password since it's using
+          * System.console.readPassword() for that.
+          */
+         assertEquals(Integer.valueOf(100), Artemis.internalExecute(null, null, new String[] {"producer", "--destination", "queue://q1", "--message-count", "100", "--password", "admin"}, context));
+
+
+         /*
+          * This is the same as above except it will prompt the user to re-enter both the URL and the username.
+          */
+         in = new ByteArrayInputStream("tcp://localhost:61616\nadmin\n".getBytes());
+         context = new ActionContext(in, System.out, System.err);
+         assertEquals(Integer.valueOf(100), Artemis.internalExecute(null, null, new String[] {"producer", "--destination", "queue://q1", "--message-count", "100", "--password", "admin", "--url", "tcp://badhost:11111"}, context));
+      } finally {
+         stopServer();
+      }
    }
 
    @Test