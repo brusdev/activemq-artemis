c257bdc9ab710707f85142c3240ab1e62509a8dc
Domenico Francesco Bruscino <brusdev@apache.org>
2023-02-17 09:11:02 +0100

    ARTEMIS-TEMP Fix console custom root redirect

--- artemis-web/src/test/java/org/apache/activemq/cli/test/WebServerComponentTest.java
+++ artemis-web/src/test/java/org/apache/activemq/cli/test/WebServerComponentTest.java
@@ -23,8 +23,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,6 +57,7 @@ import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.handler.ssl.SslHandler;
@@ -454,6 +457,45 @@ public class WebServerComponentTest extends Assert {
       Assert.assertFalse(webServerComponent.isStarted());
    }
 
+   @Test
+   public void testDefaultRootRedirect() throws Exception {
+      testRootRedirect(null, 404, null);
+   }
+
+   @Test
+   public void testCustomRootRedirect() throws Exception {
+      testRootRedirect("test-root-redirect", 302, "test-root-redirect");
+   }
+
+   public void testRootRedirect(String rootRedirectLocation, int expectedResponseCode, String expectedResponseLocation) throws Exception {
+      BindingDTO bindingDTO = new BindingDTO();
+      bindingDTO.uri = "http://localhost:0";
+      WebServerDTO webServerDTO = new WebServerDTO();
+      webServerDTO.setBindings(Collections.singletonList(bindingDTO));
+      webServerDTO.path = "";
+      webServerDTO.rootRedirectLocation = rootRedirectLocation;
+
+      WebServerComponent webServerComponent = new WebServerComponent();
+      webServerComponent.configure(webServerDTO, null, null);
+      webServerComponent.start();
+      try {
+         int port = webServerComponent.getPort(0);
+         java.net.URL url = new URL("http://localhost:" + port);
+         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+         conn.setInstanceFollowRedirects(false);
+         try {
+            assertEquals(expectedResponseCode, conn.getResponseCode());
+            if (expectedResponseLocation != null) {
+               assertTrue(conn.getHeaderField("Location").endsWith(webServerDTO.rootRedirectLocation));
+            }
+         } finally {
+            conn.disconnect();
+         }
+      } finally {
+         webServerComponent.stop(true);
+      }
+   }
+
    private void createTestWar(String warName) throws Exception {
       File warFile = new File("target", warName);
       File srcFile = new File("src/test/webapp");
@@ -577,6 +619,7 @@ public class WebServerComponentTest extends Assert {
       private CountDownLatch latch;
       private StringBuilder body = new StringBuilder();
       private String serverHeader;
+      private HttpResponse response;
 
       ClientHandler(CountDownLatch latch) {
          this.latch = latch;
@@ -585,7 +628,7 @@ public class WebServerComponentTest extends Assert {
       @Override
       public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
          if (msg instanceof HttpResponse) {
-            HttpResponse response = (HttpResponse) msg;
+            response = (HttpResponse) msg;
             serverHeader = response.headers().get("Server");
          } else if (msg instanceof HttpContent) {
             HttpContent content = (HttpContent) msg;
